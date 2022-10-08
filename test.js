61eae28a3d62c1f14ce479eaecdcefc77bb8f0df
pavantejasaikam07 <113043365+pavantejasaikam07@users.noreply.github.com>
2022-10-08 20:38:23 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const formattedDate = (d) => {
+  return d.toISOString().split("T")[0];
+};
+
+var dateToday = new Date();
+const today = formattedDate(dateToday);
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+);
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+);
+
+const expectedOutput = `My Todo-list
+Overdue
+[ ] Submit assignment ${yesterday}
